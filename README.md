f5b713412d768635fe970b2517985f2f8d5fddd1
rubixvi <vincent@rubixstudios.com.au>
2024-05-25 10:01:34 +1000

    Einstein Intial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Einstein Token on Cronos
+
+This project is a public repository of ER20 Einstein Smart Contract that can be deloyed on the Cronos chain.
+
+- OpenZepplin
+- Remix
+
+- Presale Functionality
---
+++ contracts/EIN.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Remove .deps/npm/
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Einstein is ERC20, Ownable {
+    uint256 public constant totalEIN = 500000000000000 * (10 ** 18);
+    uint256 public constant saleEIN = totalEIN / 2; 
+    uint256 public CROcontributed = 0;
+    mapping(address => uint256) public CROcontributions;
+
+    uint256 public salestarttimestamp;
+    uint256 public saleendtimestamp;
+    uint256 public claimstarttimestamp;
+
+    error salenotstarted();
+    error salealreadyended();
+    error claimnotstarted();
+    error nocontribution();
+    error invalidtimestamps();
+    error recipientsamountsmismatch();
+
+    event contributionupdated(address indexed contributor, uint256 amountcontributed, uint256 newCROcontributed);
+
+    constructor() ERC20("Einstein", "EIN") Ownable(msg.sender) {
+        _mint(address(this), totalEIN);
+    }
+
+    function setsaleperiod(uint256 _salestarttimestamp, uint256 _saleendtimestamp) public onlyOwner {
+        if (_salestarttimestamp >= _saleendtimestamp) revert invalidtimestamps();
+        salestarttimestamp = _salestarttimestamp;
+        saleendtimestamp = _saleendtimestamp;
+    }
+
+    function setclaimstarttimestamp(uint256 _claimstarttimestamp) public onlyOwner {
+        if (_claimstarttimestamp <= saleendtimestamp) revert invalidtimestamps();
+        claimstarttimestamp = _claimstarttimestamp;
+    }
+
+    function ein() public payable {
+        if (block.timestamp < salestarttimestamp || block.timestamp > saleendtimestamp) revert salenotstarted();
+        CROcontributions[msg.sender] += msg.value;
+        CROcontributed += msg.value;
+
+        emit contributionupdated(msg.sender, msg.value, CROcontributed);
+    }
+
+    function claim() public {
+        if (block.timestamp < claimstarttimestamp) revert claimnotstarted();
+        uint256 contribution = CROcontributions[msg.sender];
+        if (contribution == 0) revert nocontribution();
+
+        uint256 claimableamount = (contribution * saleEIN) / CROcontributed;
+        CROcontributions[msg.sender] = 0; 
+        _transfer(address(this), msg.sender, claimableamount);
+    }
+
+    function withdrawCRO(uint256 amount, address receiver) public onlyOwner {
+        (bool ok, ) = payable(receiver).call{value: amount}("");
+        if (!ok) revert nocontribution();
+    }
+
+    function einstein(address[] memory recipients, uint256[] memory amounts) public onlyOwner {
+        if (recipients.length != amounts.length) revert recipientsamountsmismatch();
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            _transfer(address(this), recipients[i], amounts[i]);
+        }
+    }
+}

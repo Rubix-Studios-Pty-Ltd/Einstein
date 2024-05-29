f987000255bd400ba22ef2360d9c25391d2a0b3a
rubixvi <vincent@rubixstudios.com.au>
2024-05-29 22:42:44 +1000

    git checkout --orphan latest_branch

--- contracts/EIN.sol
+++ contracts/EIN.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Remove .deps/npm/
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
